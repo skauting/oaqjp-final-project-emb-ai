c514174a25ec890925474ecd5a2e403dba66ac47
Jan Nemec <jan.nemec@gmail.com>
2024-11-13 07:54:16 -0500

    Task 7 - done

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,8 +6,20 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(URL, json = input_json, headers=header)
-    formated_response = json.loads(response.text)
-    emotions = formated_response['emotionPredictions'][0]['emotion']
-    max_emotion = max(emotions, key=emotions.get)
-    emotions['dominant_emotion'] = max_emotion
-    return emotions
+    
+    if response.status_code == 200:
+        formated_response = json.loads(response.text)
+        emotions = formated_response['emotionPredictions'][0]['emotion']
+        max_emotion = max(emotions, key=emotions.get)
+        emotions['dominant_emotion'] = max_emotion
+        return emotions
+    elif response.status_code == 400:
+        formated_response = {
+                            'anger': None,
+                            'disgust': None, 
+                            'fear': None, 
+                            'joy': None, 
+                            'sadness': None, 
+                            'dominant_emotion': None}
+        return formated_response
+    
